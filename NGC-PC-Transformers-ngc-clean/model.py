95f4e1c31496c3ddaaf0020bc775d63d972b88a1
Yohannes Alemu <alemuyohannes960@gmail.com>
2025-12-13 08:12:12 +0300

    change

--- NGC-PC-Transformers-ngc-clean/model.py
+++ NGC-PC-Transformers-ngc-clean/model.py
@@ -493,6 +493,7 @@ class NGCTransformer:
         # Assign to self attributes
         for name, node in zip(component_names, nodes):
             setattr(self, name, node)
+        print(self.nodes)            
 
         print(f"✅ Successfully loaded {len(nodes)} components for evaluation.")
 
