8320be6d99fdb467a10d580774454b46c8346052
Yohannes Alemu <alemuyohannes960@gmail.com>
2025-12-16 11:38:13 +0300

    k

--- NGC-PC-Transformers-ngc-clean/model.py
+++ NGC-PC-Transformers-ngc-clean/model.py
@@ -585,7 +585,7 @@ class NGCTransformer:
             block_proj.q_mlp2_Ratecell.z.set(  self.circuit.get_components(f"{p_prefix}_q_mlp2_Ratecell").z.get())
             # self.circuit.get_components(f"{p_prefix}_q_qkv_Ratecell").z.get()
 
-
+ #
 
             print(self.circuit.get_components(f"{p_prefix}_q_qkv_Ratecell").z.get())
 
