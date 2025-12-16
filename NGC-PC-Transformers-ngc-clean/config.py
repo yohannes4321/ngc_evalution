98bc0d79a81a5b507446ccf53b0bcd8b52303314
Yohannes Alemu <alemuyohannes960@gmail.com>
2025-12-16 11:52:28 +0300

    j

--- NGC-PC-Transformers-ngc-clean/config.py
+++ NGC-PC-Transformers-ngc-clean/config.py
@@ -13,8 +13,8 @@ class Config:
     exp_dir = "exp" 
     pos_learnable = False
     optim_type = "adam"
-    num_iter = 3
-    n_iter= 20
+    num_iter = 1
+    n_iter= 3
     wub = 0.02
     wlb = -0.02
     tau_m = 5.
