edb90eb12de4e02784029c815ceab982b757839c
Yohannes Alemu <alemuyohannes960@gmail.com>
2025-12-18 18:30:51 +0300

    h

--- NGC-PC-Transformers-ngc-clean/data_preprocess/data_loader.py
+++ NGC-PC-Transformers-ngc-clean/data_preprocess/data_loader.py
@@ -7,15 +7,22 @@ DIR = Path(__file__).parent
 sys.path.append(str(DIR.parent))
 from config import Config as config
 
+
 class DataLoader:
-    def __init__(self, data_dir= DIR / "outputs" / "tokenized_data", seq_len=config.seq_len, batch_size=config.batch_size):
+    def __init__(
+        self,
+        data_dir=DIR / "outputs" / "tokenized_data",
+        seq_len=config.seq_len,
+        batch_size=config.batch_size
+    ):
         self.data_dir = Path(data_dir)
         self.seq_len = seq_len
         self.batch_size = batch_size
         self.pad_token = 0
+        self.max_items = 50   # <<< HARD LIMIT (TOTAL SEQUENCES)
 
     def load_and_prepare_data(self):
-        """Load tokenized data and prepare for training"""
+        """Load tokenized data and prepare small datasets"""
         train_tokens = jnp.load(self.data_dir / "train_tokens.npy")
         valid_tokens = jnp.load(self.data_dir / "valid_tokens.npy")
         test_tokens = jnp.load(self.data_dir / "test_tokens.npy")
@@ -27,28 +34,32 @@ class DataLoader:
         return train_loader, valid_loader, test_loader
 
     def _create_data_loader(self, tokens, shuffle):
-        """Create sequences and return NGC DataLoader"""
-        window_size = self.seq_len + 1 
-        num_sequences = (len(tokens) - window_size + 1) // 1  
-        
+        """Create at most 50 sequences"""
+        window_size = self.seq_len + 1
+
+        max_possible = len(tokens) - window_size + 1
+        num_sequences = min(max_possible, self.max_items)
+
         if num_sequences <= 0:
             padded_tokens = jnp.concatenate([
-                tokens, 
+                tokens,
                 jnp.full((window_size - len(tokens),), self.pad_token)
             ])
-            sequences = padded_tokens.reshape(1, -1)  
+            sequences = padded_tokens.reshape(1, -1)
         else:
-            sequences = []
-            for i in range(num_sequences):
-                window = tokens[i:i + window_size]
-                sequences.append(window)
-            sequences = jnp.stack(sequences)  
-        
-        inputs = sequences[:, :-1]    
-        targets = sequences[:, 1:]    
-                
+            sequences = jnp.stack([
+                tokens[i:i + window_size]
+                for i in range(num_sequences)
+            ])
+
+        inputs = sequences[:, :-1]
+        targets = sequences[:, 1:]
+
         return NGCDataLoader(
-            design_matrices=[("inputs", inputs), ("targets", targets)],
+            design_matrices=[
+                ("inputs", inputs),
+                ("targets", targets)
+            ],
             batch_size=self.batch_size,
             disable_shuffle=not shuffle,
             ensure_equal_batches=True
